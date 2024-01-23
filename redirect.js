3dd8636969d763f88d59dd4587876167e81c0aef
JoelSmalls <joelokanta46@gmail.com>
2024-01-23 01:14:54 +0000

    first commit

---
+++ redirect.js
@@ -0,0 +1,6 @@
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.url && changeInfo.url.includes("https://www.reddit.com") && !changeInfo.url.includes("new.reddit.com")) {
+      chrome.tabs.update(tabId, {url: changeInfo.url.replace("www.reddit.com", "new.reddit.com")});
+    }
+  });
+  
